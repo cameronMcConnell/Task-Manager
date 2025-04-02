202f1003f6c2a532face5b682067f50462bd25ba
cameronMcConnell <camer@DESKTOP-AQTV1DS.>
2025-04-01 22:14:47 -0700

    Began devloping main container and comps

--- task-manager/src/App.tsx
+++ task-manager/src/App.tsx
@@ -1,10 +1,16 @@
-function App() {
+import Column from './components/column.tsx';
+import Card from './components/card.tsx';
 
+function App() {
   return (
-    <div>
-      <h1>hello world!</h1>
+    <div className='flex justify-center items-center w-screen h-screen'>
+      <div className='flex w-6/10 h-4/5 gap-4'>
+        <Column />
+        <Column />
+        <Column />
+      </div>
     </div>    
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ task-manager/src/components/column.tsx
@@ -0,0 +1,9 @@
+function Column() {
+  return (
+    <div className="grow h-full bg-amber-600">
+      <p>Column</p>
+    </div>
+  );
+}
+
+export default Column;
